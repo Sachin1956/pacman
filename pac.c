509d49dfa83b3670e522ac196db7cf98ac6b1430
Sachin <1eyedipatch@gmail.com>
2026-01-08 20:49:43 +0530

    ver 1.0-Working loop

--- pac.c
+++ pac.c
@@ -10,7 +10,7 @@ char map[10][11]={"##########",
           "#........#",
           "#........#",
           "#........#",
-          "#........#",
+          "#......g.#",
           "#........#",
           "#........#",
           "#........#",
@@ -28,7 +28,7 @@ void mapdisplay(char map[10][11]){
 
     }
 }
-int tile_check(int r, int c){
+int tile_checko(int r, int c){
     char pos = map[r][c];
     if(pos=='#')
         return 0;
@@ -41,7 +41,7 @@ int tile_check(int r, int c){
 }
 int classify(int check){
     if(check==0){
-        printf("Invalid position\n");
+        printf("Skill Issue\n");
         return 0;
     }
     else if(check==1||check==-1){
@@ -49,25 +49,52 @@ int classify(int check){
         return 1;
     }
     else if(check==2){
-        printf("YOU DIED\n");
+        printf("WOMP WOMP\n");
         return 2;
     }
     else {
         return -1;
     }
 }
-void display_perm(int perm){
-    if(perm==1){
-        mapdisplay(map);
-        return;
-    }
-    else
-        return;
-}
 void main(){
-    pc=0;
-    pr=3;
-    int check=tile_check(pr,pc);
-    int perm=classify(check);
-    display_perm(perm);
-}
+    pr=4;
+    pc=4;
+    int runno = 1;
+
+        while(runno){
+            mapdisplay(map);
+
+            char move;
+            printf("Enter move (w/a/s/d, q to quit): ");
+            scanf(" %c", &move);
+
+            if(move == 'q'){
+                printf("Quitting game...\n");
+                break;
+            }
+
+            int new_pr = pr;
+            int new_pc = pc;
+
+            if(move=='w') new_pr--;
+            else if(move=='s') new_pr++;
+            else if(move=='a') new_pc--;
+            else if(move=='d') new_pc++;
+            else{
+                printf("Invalid move\n");
+                continue;
+            }
+
+            int check = tile_checko(new_pr, new_pc);
+            int perm = classify(check);
+
+            if(perm == 1){
+                pr = new_pr;
+                pc = new_pc;
+            }
+            else if(perm == 2){
+                printf("Game Over!\n");
+                runno = 0;
+            }
+        }
+    }
